0718f7da1ac86b23338deb2c97e6cf48a91c326b
A-007481D <labidabdelmalek@gmail.com>
2025-10-09 21:51:40 +0100

    feat(persistence): add JPA repositories and service exceptions

---
+++ src/main/java/org/medicalproject/medicalproject/application/service/exception/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package org.medicalproject.medicalproject.application.service.exception;
+
+public class BusinessRuleException extends RuntimeException {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
